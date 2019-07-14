d2b344ef2d1121788d02c6b05cea189060dc2ed3
rivaldi_saputra <rivaldi.saputra@jurnal.id>
2019-07-15 01:24:25 +0700

    - Implement ErrorController - Implement job_details page - Add custom error page

--- src/main/java/it/aldi/app/controller/roles/tutor/SearchJobController.java
+++ src/main/java/it/aldi/app/controller/roles/tutor/SearchJobController.java
@@ -2,11 +2,22 @@ package it.aldi.app.controller.roles.tutor;
 
 import it.aldi.app.controller.Routes;
 import it.aldi.app.controller.rest.dto.request.SearchJobCmd;
+import it.aldi.app.controller.rest.dto.response.JobDto;
+import it.aldi.app.domain.Job;
+import it.aldi.app.exception.EntityNotFoundException;
+import it.aldi.app.service.domain.JobService;
+import it.aldi.app.util.ControllerConstant;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
@@ -15,6 +26,8 @@ public class SearchJobController {
     private static final String SEARCH_JOB_VIEW = "tutor/search_job";
     private static final String SEARCH_JOB_DETAILS_VIEW = "tutor/search_job_details";
 
+    private final JobService jobService;
+
     @GetMapping(Routes.TUTOR_SEARCH_JOB)
     public String searchJobView(Model model) {
         model.addAttribute(new SearchJobCmd());
@@ -22,8 +35,17 @@ public class SearchJobController {
     }
 
     @GetMapping(Routes.TUTOR_SEARCH_JOB_DETAILS + "/{jobId}")
-    public String searchJobDetailsView(Model model) {
-        // TOOD: create the page
+    public String searchJobDetailsView(@PathVariable("jobId")Long jobId, Model model) {
+        JobDto jobDto = JobDto.from(jobService.findOne(jobId)
+            .orElseThrow(() -> new EntityNotFoundException("Job not available")));
+
+        model.addAttribute(jobDto);
         return SEARCH_JOB_DETAILS_VIEW;
+    }
+
+    @PostMapping(Routes.TUTOR_SEARCH_JOB_DETAILS + "/{jobId}")
+    public ModelAndView applyJobDetailsView(@PathVariable("jobId")Long jobId, @Valid @ModelAttribute JobDto jobDto) {
+        // TODO: create service to apply job
+        return new ModelAndView(ControllerConstant.redirect() + Routes.TUTOR_SEARCH_JOB);
     }
 }
