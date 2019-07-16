8d947f0e55ebac77ec9467a52f9f2ded7233d336
rivaldi_saputra <rivaldi.saputra@jurnal.id>
2019-07-16 19:15:30 +0700

    Add `applyJob()` to SearchJobService

--- src/main/java/it/aldi/app/service/search/SearchJobService.java
+++ src/main/java/it/aldi/app/service/search/SearchJobService.java
@@ -1,10 +1,13 @@
 package it.aldi.app.service.search;
 
 import it.aldi.app.controller.rest.dto.response.SearchJobDto;
+import it.aldi.app.domain.Tutor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface SearchJobService {
 
     Page<SearchJobDto> searchJob(Pageable pageable);
+
+    void applyJob(Long jobId, Tutor tutor);
 }
