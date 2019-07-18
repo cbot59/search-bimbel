404641128b36fef5d986fb7c1ef8c43ef2655bbb
rivaldi_saputra <rivaldi.saputra@jurnal.id>
2019-07-18 08:50:41 +0700

    Update JobApplication - Fix constructor - Add approved field - Add query filtered by approved status

--- src/main/java/it/aldi/app/repository/JobApplicationRepository.java
+++ src/main/java/it/aldi/app/repository/JobApplicationRepository.java
@@ -2,6 +2,8 @@ package it.aldi.app.repository;
 
 import it.aldi.app.domain.JobApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -17,4 +19,8 @@ public interface JobApplicationRepository extends JpaRepository<JobApplication, 
     Optional<JobApplication> findById(Long id);
 
     List<JobApplication> findByOrganizationId(Long organizationId);
+
+    @Query("select ja from JobApplication ja where ja.organization.id =:orgId and ja.approved =:approved")
+    List<JobApplication> findByOrganizationAndAvailability(@Param("orgId") Long organization,
+                                                           @Param("approved") Boolean approved);
 }
